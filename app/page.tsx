6c878f70e3c4964ee6880bd5ff084a412b0453b7
cexyzp <arissuryana301@gmail.com>
2026-02-14 21:22:01 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState } from 'react';
+import DomeGallery from '@/components/DomeGallery';
+import InteractionFlow from '@/components/InteractionFlow';
+
+export default function Home() {
+  const [showGallery, setShowGallery] = useState(false);
+
+  const userImages = [
+    '/1.jpg',
+    '/2.jpg',
+    '/3.jpg',
+    '/4.jpg',
+    '/5.jpg',
+    '/6.jpg',
+    '/7.jpg',
+    '/8.jpg',
+    '/9.jpg',
+    '/10.jpg',
+    '/11.jpg',
+    '/12.jpg',
+    '/13.jpg',
+    '/14.jpg',
+    '/15.jpg',
+  ];
+
+  return (
+    <main className="w-screen h-screen bg-[#060010]">
+      {!showGallery ? (
+        <InteractionFlow onFlowComplete={() => setShowGallery(true)} />
+      ) : (
+        <>
+          <audio src="/pretty.mp3" autoPlay loop className="hidden" />
+          <DomeGallery
+            images={userImages}
+            fit={0.8}
+            minRadius={600}
+            maxVerticalRotationDeg={0}
+            segments={34}
+            dragDampening={2}
+            grayscale={false}
+            autoRotationSpeed={0.1}
+          />
+        </>
+      )}
+    </main>
+  );
+}
